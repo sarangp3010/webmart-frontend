eb5e623437d29aa7da21b8d7c00c8055c24d5051
HetPatel30401 <hpatel6@albany.edu>
2023-02-18 21:54:20 -0500

    Sidebar component completed, and integrated in after search page along with product component, which loads product dynamically as per given params.

--- src/components/products/products.tsx
+++ src/components/products/products.tsx
@@ -1,33 +1,47 @@
 import "../../stylesheets/pages/_products.scss"
 
 const Product = (props: any) => {
-    return (
-        <div>
-            <div className="">
-                <div className="">
-                    {/* <img width={200} height={200} src={props.data.img} alt="none"/>
-            <h5 className="card-title">{props.data.name}</h5>
-            <p className="card-text">{props.data.description}</p>
-            <p className="card-text">Price: ${props.data.price}</p>
-            <a href="#" className="btn btn-primary">Buy</a> */}
 
-                    <div className="container">
-                        <div className="card">
-                            <div className="card-header">
-                                <img src={props.data.img} alt="rover" />
+    const productRating: number = props.data.rating;
+    let starRating: Array<any> = [];
+    for (let i: number = 0; i <= productRating; i++) {
+        starRating.push(<span key={i} className="fa fa-star checked d-inline"></span>);
+    }
+
+    for (let i: number = productRating + 1; i <= 5; i++) {
+        starRating.push(<span key={i} className="fa fa-star"></span>);
+    }
+
+    const temp: Array<any> = [];
+    temp.push(<span className="fa fa-star checked"></span>);
+    temp.push(<span className="fa fa-star checked"></span>);
+    console.log("Done!!");
+
+    return (
+        <div className="main-product-list">
+            <div className="product-card">
+                <div className="container">
+                    <div className="card">
+                        <div className="card-header">
+                            <img src={props.data.img} alt="rover" />
+                        </div>
+                        <div className="card-body">
+                            <div className="d-inline first-row">
+                                <span className="tag tag-teal">{props.data.used_type}</span>
+                                <div className="text-end ms-5 d-inline">
+                                    {starRating}
+                                </div>
                             </div>
-                            <div className="card-body">
-                                <span className="tag tag-teal">{props.data.Used_type}</span>
-                                <p className="card-text">{props.data.description}</p>
-                                <p className="card-text">Price: ${props.data.price}</p>
-                                <a href="#" className="btn btn-primary">Buy</a>
-                            </div>
+                            <p className="card-text mt-3">{props.data.name}</p>
+                            <p className="card-text">Price: {props.data.price}</p>
+                            <a href="" className="btn btn-primary">Buy</a>
+
                         </div>
                     </div>
                 </div>
             </div>
-
-        </div>)
+        </div>
+    )
 };
 
 export default Product;
--- src/pages/afterSearch.tsx
+++ src/pages/afterSearch.tsx
@@ -1,19 +1,27 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import temp from '../Assets/Images/logo192.png';
 import Product from "../components/products/products";
 import data from "../data.json";
+import SideBar from "../components/sideBar/sideBar";
+import "../stylesheets/pages/_afterSearch.scss";
 
 const AfterSeach = () => {
     return (
-        <div>
-            <Row>
-                {data.map((product, i) => (
-                    <Col key={i} sm={12} md={4} lg={3} className="mb-3">
-                        <Product data={product} />
-                    </Col>
-                ))}
-            </Row>
+        <div className="after-search">
+            <div className="after-search-list-wrap">
+                <div className="side-panel-wrap">
+                    <SideBar />
+                </div>
+                <div className="list-wrap">
+                    <Row>
+                        {data.map((product, i) => (
+                            <Col key={i} sm={12} md={6} lg={4} className="">
+                                <Product data={product} />
+                            </Col>
+                        ))}
+                    </Row>
+                </div>
+            </div>
         </div>
     );
 };
