6cc478355ff5a49160903753d73f53a6608f4c20
satwikbhasin <satwikbhasin@gmail.com>
2023-04-10 11:33:47 -0400

    setting_up_contact_page_and_routing

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -66,14 +66,21 @@ const routes = [
     element: lazy(() => import("../pages/becomeSeller")),
     exact: true,
   },
-  {path: "/admin-dashboard",
-  element: lazy(() => import("../pages/admindashboard")),
-  exact: true,
-},
-{path: "/order/success",
-element: lazy(() => import("../components/orderSuccess")),
-exact: true,
-},
+  {
+    path: "/admin-dashboard",
+    element: lazy(() => import("../pages/admindashboard")),
+    exact: true,
+  },
+  {
+    path: "/order/success",
+    element: lazy(() => import("../components/orderSuccess")),
+    exact: true,
+  },
+  {
+    path: "/contactUs",
+    element: lazy(() => import("../pages/contact")),
+    exact: true,
+  },
 ];
 
 export default routes;
